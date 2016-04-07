1c70d7caea3bd2ae309a35bbeb17ddf0e2fe4bd3
NicholasBrooks <nicholas.brooks@snhu.edu>
2016-04-07 11:12:41 -0400

    Added files via upload

---
+++ Lamp.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Lamp : MonoBehaviour 
+{
+	public GameObject lamp;
+	public GameObject knew;
+
+
+	void start()
+	{
+		knew.SetActive (false);
+	}
+
+	void OnTriggerEnter ( Collider other )
+	{
+
+		if (other.gameObject.CompareTag ("Man"))
+
+		{
+			Destroy(lamp);
+			knew.SetActive (true);
+
+		}
+
+	}
+
+
+}
---
+++ Trig1.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trig1 : MonoBehaviour {
+
+
+	public Camera View1;
+	public Camera View2;
+
+	void OnTriggerEnter ( Collider other )
+	{
+
+		if (other.gameObject.CompareTag ("Man"))
+
+		{
+			View2.enabled= false;
+			View1.enabled= true;
+		}
+	
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Man"))
+
+		{
+			View2.enabled= true;
+			View1.enabled= false;
+
+
+		}
+	}
+}
